0a814a967d18ac4089c6fdbaf0bfee932a64f56a
Sebastian Mayer <sebastian.mayer@check24.de>
2024-01-10 00:52:23 +0100

    add key prop

--- src/App.tsx
+++ src/App.tsx
@@ -85,6 +85,7 @@ function App() {
         <FormSplitBill
           selectedFriend={selectedFriend}
           onSplitBill={handleSplitBill}
+          key={selectedFriend.id}
         ></FormSplitBill>
       )}
     </div>
